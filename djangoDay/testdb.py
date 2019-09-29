8632960a850d0cd829b0d13a5c4c05a21b2ccd07
DELL <15631184525@163.com>
2019-09-29 12:15:22 +0800

    test commit

--- djangoDay/testdb.py
+++ djangoDay/testdb.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from app.models import Test
 
 def testdb(request):
-    test1 = Test(name='ting')
+    test1 = Test(name='ting',age=18)
     test1.save()
     return HttpResponse('数据库djangoday1添加name成功')
